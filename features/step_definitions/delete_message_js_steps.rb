fbe99c0192c10cbc0ffda85fd970890d4b6f27bd
oliver_ochman <oliver.ochman@hotmail.com>
2017-12-01 14:22:33 +0100

    adds delete message feature with step def file

---
+++ features/step_definitions/delete_message_js_steps.rb
@@ -0,0 +1,16 @@
+Given("following users exists") do |table|
+  # table is a Cucumber::MultilineArgument::DataTable
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^I am logged\-in as "([^"]*)"$/) do |arg|
+  pending
+end
+
+And(/^I send a mail to "([^"]*)"$/) do |arg|
+  pending
+end
+
+And(/^I am on the "([^"]*)"$/) do |arg|
+  pending
+end
